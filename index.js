dd8225a19f7104d08bb0d6e645efc5a680518211
namhoang32423 <qqweqwe212nam1311@outlook.com>
2024-05-08 20:47:27 +0700

    aptos task

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-const uniqueElements = (arr) => [...new Set(arr)];
+function maxProfit(prices) {
+  let minPrice = Infinity;
+  let maxProfit = 0;
+  for (const price of prices) {
+    minPrice = Math.min(minPrice, price);
+    maxProfit = Math.max(maxProfit, price - minPrice);
+  }
+  return maxProfit;
+}
