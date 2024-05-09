38af87b78b845f6e5bd91506cdcc3eb365f8a291
namhoang32423 <qqweqwe212nam1311@outlook.com>
2024-05-09 20:23:50 +0700

    aptos sdk commit

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
-function maxProfit(prices) {
-  let minPrice = Infinity;
-  let maxProfit = 0;
-  for (const price of prices) {
-    minPrice = Math.min(minPrice, price);
-    maxProfit = Math.max(maxProfit, price - minPrice);
-  }
-  return maxProfit;
+function capitalizeWords(str) {
+  return str
+    .split(" ")
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(" ");
 }
