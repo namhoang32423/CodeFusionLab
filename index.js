5dd4a4c9470fbbc683e0f94622cc0ffd33cb2438
namhoang32423 <qqweqwe212nam1311@outlook.com>
2024-05-15 15:27:14 +0700

    update aptos commit

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
-function lengthOfLIS(nums) {
-  if (nums.length === 0) return 0;
-  const dp = new Array(nums.length).fill(1);
-  let max = 1;
-  for (let i = 1; i < nums.length; i++) {
-    for (let j = 0; j < i; j++) {
-      if (nums[i] > nums[j]) {
-        dp[i] = Math.max(dp[i], dp[j] + 1);
-        max = Math.max(max, dp[i]);
-      }
-    }
+const gcd = (a, b) => {
+  if (b === 0) {
+    return a;
   }
-  return max;
-}
+  return gcd(b, a % b);
+};
